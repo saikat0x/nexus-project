7b7398503e607bf5f4d3319b140e198bbc590259
saikat0x <saikatmajumder8436@gmail.com>
2026-08-30 19:46:34 +0530

    Update project.java
    simple calculator

--- project.java
+++ project.java
@@ -1,5 +1,43 @@
-public class Hello {
+import java.util.Scanner;
+
+public class Calculator {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter operator (+, -, *, /): ");
+        char operator = scanner.next().charAt(0);
+
+        System.out.print("Enter second number: ");
+        double num2 = scanner.nextDouble();
+
+        double result;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 == 0) {
+                    System.out.println("Error: Division by zero");
+                    return;
+                }
+                result = num1 / num2;
+                break;
+            default:
+                System.out.println("Error: Invalid operator");
+                return;
+        }
+
+        System.out.println("Result: " + result);
+        scanner.close();
     }
 }
